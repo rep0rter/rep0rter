44b78391910a5afb2f2eab8f17800990fad5117f
Sky Hong <sky9109@gmail.com>
2026-09-19 15:56:44 +0800

    Reapply "Merge pull request #40 from PGpenguin72/pr/theme-aware-image-cards"
    This reverts commit 146743b344e0b12a29c317be87db7df745239332.

--- rep0rter/templates/theme.js
+++ rep0rter/templates/theme.js
@@ -59,6 +59,7 @@
         icon.hidden = icon.dataset.themeIcon !== preference;
       });
     });
+    return window.Rep0rterImages?.sync();
   };
   const announce = (name, detail) => {
     if (typeof CustomEvent === 'function' && typeof document.dispatchEvent === 'function') {
@@ -114,12 +115,12 @@
     root.dataset.themeTransition = 'reveal';
     try {
       const transition = document.startViewTransition(() => {
-        if (revision === reveal.revision) apply();
+        if (revision === reveal.revision) return apply();
       });
       reveal.transition = transition;
       // Attach rejection handlers immediately: skipTransition rejects ready.
       transition.updateCallbackDone.catch(() => {
-        if (revision === reveal.revision) apply();
+        if (revision === reveal.revision) return apply();
       });
       transition.finished.then(() => clearReveal(reveal), () => clearReveal(reveal));
       transition.ready.then(() => {
